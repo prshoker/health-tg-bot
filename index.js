797b7d53565eb680a93ff1e193952f21a8f2d124
prshoker <31126376+prshoker@users.noreply.github.com>
2019-06-18 19:20:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ const TOKEN = '832456131:AAEdfNCmQeu5InaPZzTOGXShISJooDsnxmA'
 
 const bot = new TelegramBot(TOKEN, {polling: true})
 
-bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id, ${msg.from.first_name})
+// Написать мне ... (/echo Hello World! - пришлет сообщение с этим приветствием.)
+bot.onText(echo ('Hello!'), function (msg, match) {
+var fromId = msg.from.id;
+var resp = match[1];
+bot.sendMessage(fromId, resp);
+});
 
-})
+// Простая команда без параметров.
+bot.on('message', function (msg) {
+    var chatId = msg.chat.id;
+    // Фотография может быть: путь к файлу, поток(stream) или параметр file_id
+    var photo = 'cats.png';
+    bot.sendPhoto(chatId, photo, {caption: 'Милые котята'});
+});
