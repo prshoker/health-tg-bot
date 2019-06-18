17574828015925f8f02d53178e89e42bf09605a2
Alex Belov <belovak645@gmail.com>
2019-06-18 17:38:27 +0300

    'initial

---
+++ index.js
@@ -0,0 +1,9 @@
+const TelegramBot = require('node-telegram-bot-api')
+
+const TOKEN = '832456131:AAEdfNCmQeu5InaPZzTOGXShISJooDsnxmA'
+
+const bot = new TelegramBot(TOKEN, {polling: true})
+
+bot.on('message', msg => {
+    bot.sendMessage(msg.chat.id, 'Привет, бродяга:, ${msg.from.first_name}')
+})
