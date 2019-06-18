7ac766fd1615c191ad4e684de80062057ac919c5
prshoker <31126376+prshoker@users.noreply.github.com>
2019-06-18 18:55:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const TOKEN = '832456131:AAEdfNCmQeu5InaPZzTOGXShISJooDsnxmA'
 const bot = new TelegramBot(TOKEN, {polling: true})
 
 bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id, 'Hello from HEROKU, bot says: "Hi, ${msg.from.first_name}"')
+    bot.sendMessage(msg.chat.id, 'Приветствую' + ${msg.from.first_name} +  'asd')
 
 })
